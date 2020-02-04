4b3f6c459d45d387bd9080c58950fd055dd56879
gbudau <gbudau@student.42madrid.com>
2020-02-04 23:55:01 +0100

    Simplified the structure in header file

--- ft_printf.h
+++ ft_printf.h
@@ -5,6 +5,16 @@
 # include <stdarg.h>
 # include <stdio.h>
 # include "libft/libft.h"
+# define ALLSIGNS "-0.*123456789cspdioxX%"
+# define F_ZERO 1
+# define F_MINUS 2
+
+typedef struct s_pf
+{
+	unsigned int	flags;
+	int		precision;
+	int		width;
+}	t_pf;
 
 int	ft_printf(const char *fmt, ...);
 
