0e5730901c660a8d08bc020669112289c99bb121
gbudau <gbudau@student.42madrid.com>
2020-02-17 09:24:18 +0100

    Renamed variables

--- src/pf_char.c
+++ src/pf_char.c
@@ -6,26 +6,26 @@
 /*   By: gbudau <gbudau@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/13 02:38:18 by gbudau            #+#    #+#             */
-/*   Updated: 2020/02/17 09:01:05 by gbudau           ###   ########.fr       */
+/*   Updated: 2020/02/17 09:23:37 by gbudau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/ft_printf.h"
 
-int	pf_char(va_list *ap, t_printf *list)
+int	pf_char(va_list *ap, t_printf *var)
 {
 	int		count;
 	unsigned char	c;
 
 	c = va_arg(*ap, int);
-	if (list->flags & F_MINUS)
+	if (var->flags & F_MINUS)
 	{
 		pf_putchar(c);
-		count = pf_put_space(list->width - 1);
+		count = pf_put_space(var->width - 1);
 	}
 	else
 	{
-		count = pf_put_space(list->width - 1);
+		count = pf_put_space(var->width - 1);
 		pf_putchar(c);
 	}
 	return (count + 1);
