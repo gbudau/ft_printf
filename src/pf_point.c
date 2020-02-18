2c74b9a8aff2b86ac54b21f588a9babd359ed673
gbudau <gbudau@student.42madrid.com>
2020-02-18 20:41:01 +0100

    Changed pointer conversion to use the new functions

--- src/pf_point.c
+++ src/pf_point.c
@@ -6,51 +6,20 @@
 /*   By: gbudau <gbudau@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/16 06:29:54 by gbudau            #+#    #+#             */
-/*   Updated: 2020/02/17 09:01:18 by gbudau           ###   ########.fr       */
+/*   Updated: 2020/02/18 20:32:10 by gbudau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/ft_printf.h"
 
-static int	put_long_hex(unsigned long n)
+static int	pf_null_pointer(long n, t_printf *l)
 {
-	int		i;
-	unsigned long	t;
-
-	i = 1;
-	if (n == 0)
-		return (0);
-	i += put_long_hex(n / 16);
-	t = n % 16;
-	if (t >= 10)
-		ft_putchar('a' + (t - 10));
-	else
-		ft_putchar('0' + t);
-	return (i);
-}
-
-static int	long_hex_len(unsigned long n)
-{
-	int	i;
-
-	i = 0;
-	while (n)
-	{
-		n /= 16;
-		i++;
-	}
-	if (i == 0)
-		i++;
-	return (i);
-}
-
-static	int	combine_prefix(unsigned long n)
-{
-	int	count;
-
-	count = pf_putstrn("0x", 2);
-	count += put_long_hex(n);
-	return (count);
+	if (l->flags & F_MINUS)
+		n += pf_putstrn("(nil)", 5);
+	n += pf_put_space(l->width - 5);
+	if (!(l->flags & F_MINUS))
+		n += pf_putstrn("(nil)", 5);
+	return (n);
 }
 
 int		pf_point(va_list *ap, t_printf *l)
@@ -58,23 +27,18 @@ int		pf_point(va_list *ap, t_printf *l)
 	long		n;
 	int		count;
 	int		len;
+	char		buffer[15];
 	
 	n = (long)va_arg(*ap, void *);
 	if (n == 0)
-	{
-		n += pf_put_space(l->width - 5);
-		return (n += pf_putstrn("(nil)", 5));
-	}
-	len = long_hex_len(n) + 2;
+		return (n += pf_null_pointer(n, l));
+	count = 0;
+	ft_strcpy(buffer, "0x");
+	len = pf_ultoa_base_len(n, &buffer[2], 16, 0) + 2;
 	if (l->flags & F_MINUS)
-	{
-		count = combine_prefix(n);
-		count += pf_put_space(l->width - len);
-	}
-	else
-	{
-		count = pf_put_space(l->width - len);
-		count += combine_prefix(n);
-	}
+		count += pf_putstrn(buffer, len);
+	count += pf_put_space(l->width - len);
+	if (!(l->flags & F_MINUS))
+		count += pf_putstrn(buffer, len);
 	return (count);	
 }
