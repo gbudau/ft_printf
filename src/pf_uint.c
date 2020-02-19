235d87e6fbe835f7c510fe5bbeab0eb2734e4787
gbudau <gbudau@student.42madrid.com>
2020-02-19 12:52:23 +0100

    Modified unsigned conversion and deleted hex_upper, not needed anymore

--- src/pf_uint.c
+++ src/pf_uint.c
@@ -6,70 +6,35 @@
 /*   By: gbudau <gbudau@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/16 14:01:48 by gbudau            #+#    #+#             */
-/*   Updated: 2020/02/19 11:54:03 by gbudau           ###   ########.fr       */
+/*   Updated: 2020/02/19 12:50:51 by gbudau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/ft_printf.h"
 
-static int	put_uint(unsigned int n)
-{
-	int		i;
-	unsigned int	t;
-	char		buffer[11];
-
-	i = 0;
-	while (n != 0)
-	{
-		t = n % 10;
-		buffer[i++] = t  + '0';
-		n /= 10;
-	}
-	if (i == 0)
-		buffer[i++] = '0';
-	buffer[i] = '\0';
-	ft_strnrevn(buffer, 0, i - 1);
-	pf_putstrn(buffer, i);
-	return (i);
-}
-
-static int	uint_len(unsigned int n)
-{
-	int	i;
-	
-	i = 0;
-	while (n != 0)
-	{
-		n /= 10;
-		i++;
-	}
-	if (i == 0)
-		i++;
-	return (i);
-}
-
 int		pf_uint(va_list *ap, t_printf *s, int out)
 {
 	unsigned int	n;
 	int		len;
+	char		buffer[11];
 
-	n = va_arg(*ap, unsigned int);
+	n = va_arg(*ap, int);
 	if (s->prec == 0 && n == 0)
 		return (pf_put_space(s->width));
-	len = uint_len(n);
+	len = pf_ultoa_base_len(n, buffer, 10, 0);
 	if (s->prec < len && s->width < len)
-		return (put_uint(n));
+		return (pf_putstrn(buffer, len));
 	if (s->flags & F_MINUS)
 	{
 		out = pf_put_zero(s->prec - len);
-		out += put_uint(n);
+		out += pf_putstrn(buffer, len);
 		out += pf_put_space(s->width - (s->prec < len ? len : s->prec));
 	}
 	else
 	{
 		out = pf_put_zero_or_space(s, s->width - (s->prec < len ? len : s->prec));
 		out += pf_put_zero(s->prec - len);
-		out += put_uint(n);
+		out += pf_putstrn(buffer, len);
 	}
 	return (out);
 }
