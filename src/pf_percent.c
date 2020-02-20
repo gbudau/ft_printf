15d1fb2e0eed500f2d81ce99fe321708c0a84133
gbudau <gbudau@student.42madrid.com>
2020-02-20 12:43:43 +0100

    Percent conversion is the same as char conversion now

---
+++ src/pf_percent.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pf_percent.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gbudau <gbudau@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/20 11:52:41 by gbudau            #+#    #+#             */
+/*   Updated: 2020/02/20 12:18:31 by gbudau           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/ft_printf.h"
+
+int	pf_percent(va_list *ap, t_printf *s, int count)
+{
+	(void)ap;
+	if (s->flags & F_MINUS)
+	{
+		pf_putchar('%');
+		count += pf_put_space(s->width - 1);
+	}
+	else
+	{
+		count = pf_put_space(s->width - 1);
+		pf_putchar('%');
+	}
+	return (count + 1);
+}
