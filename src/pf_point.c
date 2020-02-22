660e07c08eb9456746d3c6a5bcc8e20e375ce534
George Adrian Budau <gbudau@student.42madrid.com>
2020-02-22 11:15:33 +0100

    Fixed a case in pointer conversion

--- src/pf_point.c
+++ src/pf_point.c
@@ -6,11 +6,28 @@
 /*   By: gbudau <gbudau@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/16 16:29:54 by gbudau            #+#    #+#             */
-/*   Updated: 2020/02/22 10:49:26 by gbudau           ###   ########.fr       */
+/*   Updated: 2020/02/22 11:10:12 by gbudau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/ft_printf.h"
+
+static int	pointer_special_case(t_printf *s)
+{
+	int			count;
+
+	if (s->flags & PF_FLAG_LEFT)
+	{
+		count = pf_putstrn("0x", 2);
+		count += pf_put_space(s->width -2);
+	}
+	else
+	{
+		count = pf_put_space(s->width - 2);
+		count += pf_putstrn("0x", 2);
+	}
+	return (count);
+}
 
 int			pf_point(va_list *ap, t_printf *s, int count)
 {
@@ -20,11 +37,7 @@ int			pf_point(va_list *ap, t_printf *s, int count)
 
 	n = (long)va_arg(*ap, void *);
 	if (n == 0 && s->prec == 0)
-	{
-		count = pf_put_space(s->width - 2);
-		count += pf_putstrn("0x", 2);
-		return (count);
-	}
+		return (pointer_special_case(s));
 	len = pf_ultoa_base_len(n, buffer, 16, count);
 	if (s->flags & PF_FLAG_LEFT)
 	{
